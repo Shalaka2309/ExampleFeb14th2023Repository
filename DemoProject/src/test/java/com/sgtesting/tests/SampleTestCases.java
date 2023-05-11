b9300c57e02945e87e45625edb8a7d134798771a
Shalaka2309 <shalaka.brahmavar@gmail.com>
2023-05-11 17:52:23 +0530

    Initial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/SampleTestCases.java
@@ -0,0 +1,18 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTestCases {
+	@Test
+	public void method1()
+	{
+		System.out.println("It is method1 test case Statement");
+	}
+	@Test
+	public void method2()
+	{
+		System.out.println("It is method2 test case Statement");
+	}
+
+
+}
